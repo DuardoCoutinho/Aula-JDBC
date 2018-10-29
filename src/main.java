428a7e175fc726d9b551b5549bceccb9ff92c0af
eduardo <eduardcoutinho2@gmail.com>
2018-10-29 18:07:56 -0300

    Isert into okay

--- src/main.java
+++ src/main.java
@@ -8,18 +8,46 @@ import java.sql.PreparedStatement;
 
 
 public class main {
-    public static void main(String[] args) {
+    private static Connection mysql;
+
+    public static void main(String[] args) throws SQLException {
         // TODO code application logic here
+
+        
         try{
-            Connection mysql = DriverManager.getConnection(
+            mysql = DriverManager.getConnection(
                     "jdbc:mysql://localhost/agenda",
                     "root",
                     "");
             System.out.println("Conectado!");
+
+            inserir();
         }catch(SQLException e){
             System.out.println("Ocorreu um erro!");
             throw new RuntimeException(e.getMessage());
 
-        }
+        }}
+    public static void inserir(){
+
+
+            String sql = "insert into contato (nome, endereco, data, email) values(?, ?, ?, ?)";
+            PreparedStatement instrucao = mysql.prepareStatement(sql);
+
+            instrucao.setString(1, "José Eduardo");
+            instrucao.setString(2, "Rua rua");
+            instrucao.setString(3, "28/06");
+            instrucao.setString(4, "eduardcoutinho2@gmail.com");
+
+            instrucao.execute();
+
+
+
+
+
+
     }
+
+
+
+
 }
