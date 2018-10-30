cc7e800157ca99233013bbf9644106d6cf1eb009
eduardo <eduardcoutinho2@gmail.com>
2018-10-30 00:07:23 -0300

    Com insert, Update e Select

--- src/main.java
+++ src/main.java
@@ -1,10 +1,8 @@
 
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.PreparedStatement;
+import com.mysql.cj.protocol.Resultset;
 
+import java.sql.*;
 
 
 public class main {
@@ -21,7 +19,9 @@ public class main {
                     "");
             System.out.println("Conectado!");
 
-            inserir();
+
+            update();
+
         }catch(SQLException e){
             System.out.println("Ocorreu um erro!");
             throw new RuntimeException(e.getMessage());
@@ -29,16 +29,75 @@ public class main {
         }}
     public static void inserir(){
 
+        try {
+
 
             String sql = "insert into contato (nome, endereco, data, email) values(?, ?, ?, ?)";
+
             PreparedStatement instrucao = mysql.prepareStatement(sql);
 
             instrucao.setString(1, "José Eduardo");
             instrucao.setString(2, "Rua rua");
             instrucao.setString(3, "28/06");
             instrucao.setString(4, "eduardcoutinho2@gmail.com");
-
             instrucao.execute();
+
+        }catch (SQLException e){
+
+            System.out.println("Ocorreu um erro!");
+            throw new RuntimeException(e.getMessage());
+
+    }
+        }
+
+        public static void select(){
+        try{
+            String sql = "SELECT * FROM contato";
+            Statement instrucao = mysql.createStatement();
+
+            Resultset resul = (Resultset) instrucao.executeQuery(sql);
+
+            while (((ResultSet) resul).next()){
+                String nome = ((ResultSet) resul).getString("nome");
+                String endereco = ((ResultSet) resul).getString("endereco");
+                String data = ((ResultSet) resul).getString("data");
+                String email = ((ResultSet) resul).getString("email");
+                System.out.println(nome + " ");
+                System.out.print(endereco+ " ");
+                System.out.print(data+ " ");
+                System.out.print(email+ " ");
+
+            }
+
+        }catch (SQLException e){
+            System.out.println("Ocorreu um Erro!");
+            throw new RuntimeException(e.getMessage());
+        }
+
+        }
+
+        public static void update(){
+            try {
+
+                String sql = "UPDATE contato SET nome = ? WHERE id_contato = 1";
+
+                PreparedStatement instrucao = mysql.prepareStatement(sql);
+
+                instrucao.setString(1, "Joana LELE");
+
+                instrucao.execute();
+
+
+            }catch (SQLException e){
+                System.out.println("Ocorreu um Erro!");
+                throw new RuntimeException(e.getMessage());
+            }
+
+
+        }
+
+
+
 
 
 
@@ -50,4 +109,6 @@ public class main {
 
 
 
-}
+
+
+
